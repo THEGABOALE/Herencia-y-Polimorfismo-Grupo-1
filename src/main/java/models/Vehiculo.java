3b06031ade5aa419b26b1d23e50d52b518ab6891
c04o <c04o@proton.me>
2025-09-23 12:05:37 -0600

    Agregar clase abstracta Vehiculo al paquete models

---
+++ src/main/java/models/Vehiculo.java
@@ -0,0 +1,21 @@
+package models;
+
+public abstract class Vehiculo {
+    protected String marca;
+    protected String modelo;
+
+    public Vehiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public abstract void mover();
+}
